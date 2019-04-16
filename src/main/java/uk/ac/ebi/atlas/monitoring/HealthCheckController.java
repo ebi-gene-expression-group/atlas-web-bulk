a2d07ecb17dbcd427b60f2dec0de0eb30f0e4d0f
amunozpomer <amunoz@ebi.ac.uk>
2019-04-16 17:15:03 +0100

    Replace old analytics with bulk-analytics alias pointing at bulk-analytics-vX

--- src/main/java/uk/ac/ebi/atlas/monitoring/HealthCheckController.java
+++ src/main/java/uk/ac/ebi/atlas/monitoring/HealthCheckController.java
@@ -1,5 +1,6 @@
 package uk.ac.ebi.atlas.monitoring;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 import uk.ac.ebi.atlas.experimentimport.GxaExperimentDao;
 
 import javax.inject.Inject;
-import java.util.Arrays;
 
 import static uk.ac.ebi.atlas.utils.GsonProvider.GSON;
 
@@ -23,13 +23,13 @@ public final class HealthCheckController {
         this.experimentDao = experimentDao;
     }
 
-    @GetMapping(
-            value = "/json/health",
-            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @GetMapping(value = "/json/health",
+                produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public String getHealthStatus() {
         return GSON.toJson(ImmutableMap.of(
-                "solr", healthCheckService.isSolrUp(Arrays.asList("analytics", "bioentities")) ? "UP" : "DOWN",
-                "db", healthCheckService.isDatabaseUp(experimentDao) ? "UP" : "DOWN"
-        ));
+                "solr",
+                healthCheckService.isSolrUp(ImmutableList.of("bioentities"), "bulk-analytics") ? "UP" : "DOWN",
+                "db",
+                healthCheckService.isDatabaseUp(experimentDao) ? "UP" : "DOWN"));
     }
 }
