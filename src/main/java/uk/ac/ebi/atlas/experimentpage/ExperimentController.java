e75a793d4132dab1dc9fbf1454bf20c26c87251a
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2019-05-31 17:18:08 +0100

    Hotfix #166377504 – GTEx times out (#3)
    * Generate production bundles
    
    * Refactor and cache the content field in the experiment page, passed to the React component as props
    
    * Don’t query for assay groups if there are none (!)

--- src/main/java/uk/ac/ebi/atlas/experimentpage/ExperimentController.java
+++ src/main/java/uk/ac/ebi/atlas/experimentpage/ExperimentController.java
@@ -1,65 +1,26 @@
 package uk.ac.ebi.atlas.experimentpage;
 
-import com.google.common.collect.ImmutableSetMultimap;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonPrimitive;
-import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import uk.ac.ebi.atlas.commons.readers.TsvStreamer;
 import uk.ac.ebi.atlas.controllers.HtmlExceptionHandlingController;
-import uk.ac.ebi.atlas.experimentpage.columngroups.HeatmapFilterGroup;
-import uk.ac.ebi.atlas.experimentpage.json.JsonBaselineExperimentController;
-import uk.ac.ebi.atlas.experimentpage.qc.MicroarrayQcFiles;
-import uk.ac.ebi.atlas.experimentpage.qc.QcReportController;
-import uk.ac.ebi.atlas.experimentpage.tsne.TSnePoint;
-import uk.ac.ebi.atlas.model.experiment.sample.ReportsGeneExpression;
-import uk.ac.ebi.atlas.model.download.ExternallyAvailableContent;
+import uk.ac.ebi.atlas.experimentpage.content.ExperimentPageContentService;
 import uk.ac.ebi.atlas.model.experiment.Experiment;
-import uk.ac.ebi.atlas.model.experiment.ExperimentDesignTable;
-import uk.ac.ebi.atlas.model.experiment.ExperimentType;
-import uk.ac.ebi.atlas.resource.DataFileHub;
-import uk.ac.ebi.atlas.search.SemanticQueryTerm;
 import uk.ac.ebi.atlas.trader.ExperimentTrader;
-
-import javax.inject.Inject;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import static uk.ac.ebi.atlas.experimentpage.columngroups.HeatmapGroupingsService.getExperimentVariablesAsHeatmapFilterGroups;
 
 @Controller
 public class ExperimentController extends HtmlExceptionHandlingController {
-    private final static Gson GSON = new GsonBuilder()
-            .registerTypeAdapter(
-                    SemanticQueryTerm.create("").getClass(),
-                    SemanticQueryTerm.getGsonTypeAdapter())
-            .registerTypeAdapter(
-                    TSnePoint.create(0.0, 0.0, "").getClass(),
-                    TSnePoint.getGsonTypeAdapter())
-            .registerTypeAdapter(
-                    HeatmapFilterGroup.create("", true, "", ImmutableSetMultimap.of()).getClass(),
-                    HeatmapFilterGroup.getGsonTypeAdapter())
-            .serializeSpecialFloatingPointValues()
-            .create();
-
     private final ExperimentTrader experimentTrader;
-    private final DataFileHub dataFileHub;
     private final ExperimentAttributesService experimentAttributesService;
+    private final ExperimentPageContentService experimentPageContentService;
 
-    @Inject
     public ExperimentController(ExperimentTrader experimentTrader,
-                                DataFileHub dataFileHub,
+                                ExperimentPageContentService experimentPageContentService,
                                 ExperimentAttributesService experimentAttributesService) {
         this.experimentTrader = experimentTrader;
-        this.dataFileHub = dataFileHub;
+        this.experimentPageContentService = experimentPageContentService;
         this.experimentAttributesService = experimentAttributesService;
     }
 
@@ -72,192 +33,10 @@ public class ExperimentController extends HtmlExceptionHandlingController {
         Experiment<?> experiment = experimentTrader.getExperiment(experimentAccession, accessKey);
 
         model.addAllAttributes(experimentAttributesService.getAttributes(experiment));
-        model.addAttribute("content", GSON.toJson(experimentPageContentForExperiment(experiment, accessKey)));
+        model.addAttribute(
+                "content",
+                experimentPageContentService.jsonSerializeContentForExperiment(experiment, accessKey));
 
         return "experiment-page";
     }
-
-    private JsonObject experimentPageContentForExperiment(final Experiment<? extends ReportsGeneExpression> experiment,
-                                                          final String accessKey) {
-        JsonObject result = new JsonObject();
-
-        // the client can't know that otherwise and it needs that!
-        result.addProperty("experimentAccession", experiment.getAccession());
-        result.addProperty("experimentType", experiment.getType().name());
-        result.addProperty("accessKey", accessKey);
-        result.addProperty("species", experiment.getSpecies().getReferenceName());
-        result.addProperty("disclaimer", experiment.getDisclaimer());
-
-        JsonArray availableTabs = new JsonArray();
-        // everything wants to have a heatmap
-        availableTabs.add(
-                heatmapTab(
-                        GSON.toJsonTree(getExperimentVariablesAsHeatmapFilterGroups(experiment)).getAsJsonArray(),
-                        JsonBaselineExperimentController.geneDistributionUrl(
-                                experiment.getAccession(),
-                                accessKey,
-                                experiment.getType()),
-                        availableDataUnits(
-                                experiment.getAccession(),
-                                experiment.getType())));
-
-        if (experiment.getType().isDifferential()) {
-            availableTabs.add(
-                    customContentTab(
-                            "resources",
-                            "Plots",
-                            "url",
-                            new JsonPrimitive(
-                                    ExternallyAvailableContentService.listResourcesUrl(
-                                            experiment.getAccession(),
-                                            accessKey,
-                                            ExternallyAvailableContent.ContentType.PLOTS))));
-        }
-
-        if (dataFileHub.getExperimentFiles(experiment.getAccession()).experimentDesign.exists()) {
-            availableTabs.add(
-                    experimentDesignTab(new ExperimentDesignTable(experiment).asJson(),
-                            ExperimentDesignFile.makeUrl(experiment.getAccession(), accessKey)));
-        }
-
-        availableTabs.add(
-                customContentTab(
-                        "multipart",
-                        "Supplementary Information",
-                        "sections",
-                        supplementaryInformationTabs(experiment, accessKey)));
-
-        availableTabs.add(
-                customContentTab(
-                        "resources",
-                        "Downloads",
-                        "url",
-                        new JsonPrimitive(ExternallyAvailableContentService.listResourcesUrl(
-                                experiment.getAccession(), accessKey, ExternallyAvailableContent.ContentType.DATA))));
-
-        result.add("tabs", availableTabs);
-
-        return result;
-    }
-
-    private JsonArray availableDataUnits(String experimentAccession, ExperimentType experimentType) {
-        if (!experimentType.isRnaSeqBaseline()) {
-            return new JsonArray();
-        } else {
-            return GSON.toJsonTree(
-                    dataFileHub.getRnaSeqBaselineExperimentFiles(experimentAccession).dataFiles()).getAsJsonArray();
-        }
-    }
-
-    private JsonArray supplementaryInformationTabs(final Experiment experiment, final String accessKey) {
-        JsonArray supplementaryInformationTabs = new JsonArray();
-        if (dataFileHub.getExperimentFiles(experiment.getAccession()).analysisMethods.exists()) {
-            try (TsvStreamer tsvStreamer =
-                         dataFileHub.getExperimentFiles(experiment.getAccession()).analysisMethods.get()) {
-                supplementaryInformationTabs.add(
-                        customContentTab(
-                                "static-table",
-                                "Analysis Methods",
-                                "data",
-                                formatTable(tsvStreamer.get().collect(Collectors.toList()))));
-            }
-        }
-
-        supplementaryInformationTabs.add(
-                customContentTab(
-                        "resources",
-                        "Resources",
-                        "url",
-                        new JsonPrimitive(
-                                ExternallyAvailableContentService.listResourcesUrl(
-                                        experiment.getAccession(),
-                                        accessKey,
-                                        ExternallyAvailableContent.ContentType.SUPPLEMENTARY_INFORMATION)))
-        );
-
-        if (experiment.getType().isMicroarray() &&
-                dataFileHub.getExperimentFiles(experiment.getAccession()).qcFolder.existsAndIsNonEmpty()) {
-            supplementaryInformationTabs.add(
-                    customContentTab(
-                            "qc-report",
-                            "QC Report",
-                            "reports",
-                            pairsToArrayOfObjects(
-                                    "name",
-                                    "url",
-                                    new MicroarrayQcFiles(
-                                            dataFileHub.getExperimentFiles(experiment.getAccession()).qcFolder)
-                                            .getArrayDesignsThatHaveQcReports().stream()
-                                                .map(arrayDesign ->
-                                                        Pair.of(
-                                                                "QC for array design " + arrayDesign,
-                                                                QcReportController.getQcReportUrl(
-                                                                        experiment.getAccession(),
-                                                                        arrayDesign,
-                                                                        accessKey)))
-                                                .collect(Collectors.toList()))));
-        }
-
-        return supplementaryInformationTabs;
-    }
-
-    private JsonArray formatTable(List<String[]> rows) {
-        JsonArray result = new JsonArray();
-        for (String[] row : rows) {
-            //skip empty rows and other unexpected input
-            if (row.length == 2) {
-                result.add(twoElementArray(row[0], row[1]));
-            }
-        }
-
-        return result;
-    }
-
-    private JsonArray pairsToArrayOfObjects(String leftName, String rightName, List<Pair<String, String>> pairs) {
-        JsonArray result = new JsonArray();
-        for (Pair<String, String> p : pairs) {
-            JsonObject o = new JsonObject();
-            o.addProperty(leftName, p.getLeft());
-            o.addProperty(rightName, p.getRight());
-            result.add(o);
-        }
-        return result;
-    }
-
-    private JsonArray twoElementArray(String x, String y) {
-        JsonArray result = new JsonArray();
-        result.add(new JsonPrimitive(x));
-        result.add(new JsonPrimitive(y));
-        return result;
-    }
-
-    private JsonObject customContentTab(String tabType, String name, String onlyPropName, JsonElement value) {
-        JsonObject props =  new JsonObject();
-        props.add(onlyPropName, value);
-        return customContentTab(tabType, name, props);
-    }
-
-    private JsonObject customContentTab(String tabType, String name, JsonObject props) {
-        JsonObject result = new JsonObject();
-        result.addProperty("type", tabType);
-        result.addProperty("name", name);
-        result.add("props", props);
-        return result;
-    }
-
-    private JsonObject heatmapTab(JsonArray groups, String geneDistributionUrl, JsonArray availableDataUnits) {
-        JsonObject props = new JsonObject();
-        props.add("groups", groups);
-        props.addProperty("genesDistributedByCutoffUrl", geneDistributionUrl);
-        props.add("availableDataUnits", availableDataUnits);
-        return customContentTab("heatmap", "Results", props);
-    }
-
-    private JsonObject experimentDesignTab(JsonObject table, String downloadUrl) {
-        JsonObject props = new JsonObject();
-        props.add("table", table);
-        props.addProperty("downloadUrl", downloadUrl);
-        return customContentTab("experiment-design", "Experiment Design", props);
-    }
-
 }
