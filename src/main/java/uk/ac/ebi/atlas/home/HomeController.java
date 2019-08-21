942892a35f04e5161fa802ec60a2e003504026ff
amunozpomer <amunoz@ebi.ac.uk>
2019-08-21 13:35:39 +0100

    Inherit from HtmlExceptionHandlingController and inject revamped ExperimentInfoListService

--- src/main/java/uk/ac/ebi/atlas/home/HomeController.java
+++ src/main/java/uk/ac/ebi/atlas/home/HomeController.java
@@ -1,39 +1,27 @@
 package uk.ac.ebi.atlas.home;
 
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import uk.ac.ebi.atlas.controllers.HtmlExceptionHandlingController;
 import uk.ac.ebi.atlas.experiments.ExperimentInfoListService;
 import uk.ac.ebi.atlas.species.AtlasInformationDao;
 import uk.ac.ebi.atlas.species.SpeciesProperties;
 import uk.ac.ebi.atlas.species.SpeciesPropertiesTrader;
-import uk.ac.ebi.atlas.trader.ExperimentTrader;
 import uk.ac.ebi.atlas.utils.ExperimentInfo;
 
 import java.util.Random;
 
 import static com.google.common.collect.ImmutableMap.toImmutableMap;
-import static uk.ac.ebi.atlas.model.experiment.ExperimentType.MICROARRAY_1COLOUR_MICRORNA_DIFFERENTIAL;
-import static uk.ac.ebi.atlas.model.experiment.ExperimentType.MICROARRAY_1COLOUR_MRNA_DIFFERENTIAL;
-import static uk.ac.ebi.atlas.model.experiment.ExperimentType.MICROARRAY_2COLOUR_MRNA_DIFFERENTIAL;
-import static uk.ac.ebi.atlas.model.experiment.ExperimentType.PROTEOMICS_BASELINE;
-import static uk.ac.ebi.atlas.model.experiment.ExperimentType.RNASEQ_MRNA_BASELINE;
-import static uk.ac.ebi.atlas.model.experiment.ExperimentType.RNASEQ_MRNA_DIFFERENTIAL;
 import static uk.ac.ebi.atlas.species.AtlasInformationDataType.EFO;
 import static uk.ac.ebi.atlas.species.AtlasInformationDataType.EG;
 import static uk.ac.ebi.atlas.species.AtlasInformationDataType.ENSEMBL;
 import static uk.ac.ebi.atlas.species.AtlasInformationDataType.WBPS;
 
 @Controller
-public class HomeController {
-    @Autowired
-    protected Environment env;
-
+public class HomeController extends HtmlExceptionHandlingController {
     // From e.g. https://www.ebi.ac.uk/s4/identification?term=tpi1
     private static final ImmutableMap<String, String> S4_SPECIES =
             ImmutableMap.of(
@@ -52,21 +40,11 @@ public class HomeController {
     private final ExperimentInfoListService experimentInfoListService;
 
     public HomeController(SpeciesPropertiesTrader speciesPropertiesTrader,
-                          ExperimentTrader experimentTrader,
-                          AtlasInformationDao atlasInformationDao) {
+                          AtlasInformationDao atlasInformationDao,
+                          ExperimentInfoListService experimentInfoListService) {
         this.speciesPropertiesTrader = speciesPropertiesTrader;
         this.atlasInformationDao = atlasInformationDao;
-
-        this.experimentInfoListService =
-                new ExperimentInfoListService(
-                        experimentTrader,
-                        ImmutableList.of(
-                                RNASEQ_MRNA_BASELINE,
-                                PROTEOMICS_BASELINE,
-                                RNASEQ_MRNA_DIFFERENTIAL,
-                                MICROARRAY_1COLOUR_MRNA_DIFFERENTIAL,
-                                MICROARRAY_2COLOUR_MRNA_DIFFERENTIAL,
-                                MICROARRAY_1COLOUR_MICRORNA_DIFFERENTIAL));
+        this.experimentInfoListService = experimentInfoListService;
     }
 
     @RequestMapping(value = "/home", produces = "text/html;charset=UTF-8")
