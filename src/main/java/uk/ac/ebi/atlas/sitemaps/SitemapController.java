8b9bdc267dfbc40c50d1a96589bf43b55078a329
amunozpomer <amunoz@ebi.ac.uk>
2019-08-29 12:32:47 +0100

    Make the controller a singleton and create a SitemapWriter instance per request

--- src/main/java/uk/ac/ebi/atlas/sitemaps/SitemapController.java
+++ src/main/java/uk/ac/ebi/atlas/sitemaps/SitemapController.java
@@ -1,31 +1,24 @@
 package uk.ac.ebi.atlas.sitemaps;
 
 import com.google.common.collect.ImmutableList;
-import org.springframework.context.annotation.Scope;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import uk.ac.ebi.atlas.solr.analytics.AnalyticsSearchService;
 import uk.ac.ebi.atlas.species.SpeciesFactory;
 import uk.ac.ebi.atlas.species.SpeciesPropertiesTrader;
 
-import javax.inject.Inject;
 import javax.servlet.http.HttpServletResponse;
-import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.stream.XMLStreamException;
 import java.io.IOException;
-import java.util.Collection;
 
 @Controller
-@Scope("request")
 public class SitemapController {
-    private final SitemapWriter sitemapWriter = new SitemapWriter();
     private final AnalyticsSearchService solr;
     private final SpeciesFactory speciesFactory;
     private final SpeciesPropertiesTrader speciesPropertiesTrader;
 
-    @Inject
     public SitemapController(AnalyticsSearchService solr,
                              SpeciesFactory speciesFactory,
                              SpeciesPropertiesTrader speciesPropertiesTrader) {
@@ -34,24 +27,21 @@ public class SitemapController {
         this.speciesPropertiesTrader = speciesPropertiesTrader;
     }
 
-
-    @RequestMapping(value = "/sitemap.xml")
+    @GetMapping(value = "/sitemap.xml")
     public void mainSitemap(HttpServletResponse response) throws IOException, XMLStreamException {
         response.setContentType(MediaType.TEXT_XML_VALUE);
-        sitemapWriter.writeSitemapIndex(response.getOutputStream(), speciesPropertiesTrader.getAll());
+        new SitemapWriter().writeSitemapIndex(response.getOutputStream(), speciesPropertiesTrader.getAll());
     }
 
-    @RequestMapping(value = "/species/{species}/sitemap.xml")
+    @GetMapping(value = "/species/{species}/sitemap.xml")
     public void sitemapForSpecies(@PathVariable String species,
                                   HttpServletResponse response) throws IOException, XMLStreamException {
-
         response.setContentType(MediaType.TEXT_XML_VALUE);
-        Collection<String> various = ImmutableList.of("/experiments", "/plant/experiments");
-
-        sitemapWriter.writeGenes(
-                response.getOutputStream(), various,
-                solr.getBioentityIdentifiersForSpecies(speciesFactory.create(species).getReferenceName()));
-
+        new SitemapWriter()
+                .writeGenes(
+                        response.getOutputStream(),
+                        ImmutableList.of("/experiments", "/plant/experiments"),
+                        solr.getBioentityIdentifiersForSpecies(speciesFactory.create(species).getReferenceName()));
     }
 
 }
