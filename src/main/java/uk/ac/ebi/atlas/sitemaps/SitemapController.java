983abd3726aef226054439eee0d1eaa23f133470
amunozpomer <amunoz@ebi.ac.uk>
2019-08-30 14:17:05 +0100

    Use new SolrCloud DAO and add endpoint for experiments sitemap

--- src/main/java/uk/ac/ebi/atlas/sitemaps/SitemapController.java
+++ src/main/java/uk/ac/ebi/atlas/sitemaps/SitemapController.java
@@ -1,49 +1,47 @@
 package uk.ac.ebi.atlas.sitemaps;
 
-import com.google.common.collect.ImmutableList;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import uk.ac.ebi.atlas.solr.analytics.AnalyticsSearchService;
-import uk.ac.ebi.atlas.species.SpeciesFactory;
-import uk.ac.ebi.atlas.species.SpeciesPropertiesTrader;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.xml.stream.XMLStreamException;
 import java.io.IOException;
 
-import static uk.ac.ebi.atlas.sitemaps.SitemapWriter.writeGenes;
+import static uk.ac.ebi.atlas.sitemaps.SitemapWriter.writeExperimentsSitemap;
+import static uk.ac.ebi.atlas.sitemaps.SitemapWriter.writeBioentityIdentifiersSitemap;
 import static uk.ac.ebi.atlas.sitemaps.SitemapWriter.writeSitemapIndex;
 
 @Controller
 public class SitemapController {
-    private final AnalyticsSearchService solr;
-    private final SpeciesFactory speciesFactory;
-    private final SpeciesPropertiesTrader speciesPropertiesTrader;
+    private final SitemapDao sitemapDao;
 
-    public SitemapController(AnalyticsSearchService solr,
-                             SpeciesFactory speciesFactory,
-                             SpeciesPropertiesTrader speciesPropertiesTrader) {
-        this.solr = solr;
-        this.speciesFactory = speciesFactory;
-        this.speciesPropertiesTrader = speciesPropertiesTrader;
+    public SitemapController(SitemapDao sitemapDao) {
+        this.sitemapDao = sitemapDao;
     }
 
     @GetMapping(value = "/sitemap.xml")
     public void mainSitemap(HttpServletResponse response) throws IOException, XMLStreamException {
         response.setContentType(MediaType.TEXT_XML_VALUE);
-        writeSitemapIndex(response.getOutputStream(), speciesPropertiesTrader.getAll());
+        writeSitemapIndex(response.getOutputStream(), sitemapDao.getSpeciesInPublicExperiments());
+    }
+
+    @GetMapping(value = "/experiments/sitemap.xml")
+    public void experimentsSitemap(HttpServletResponse response) throws IOException, XMLStreamException {
+        response.setContentType(MediaType.TEXT_XML_VALUE);
+        writeExperimentsSitemap(response.getOutputStream());
     }
 
     @GetMapping(value = "/species/{species}/sitemap.xml")
-    public void sitemapForSpecies(@PathVariable String species,
-                                  HttpServletResponse response) throws IOException, XMLStreamException {
+    public void speciesGenesSitemap(@PathVariable String species,
+                                    @RequestParam(defaultValue = "false", required = false) boolean allEntries,
+                                    HttpServletResponse response) throws IOException, XMLStreamException {
         response.setContentType(MediaType.TEXT_XML_VALUE);
-        writeGenes(
+        writeBioentityIdentifiersSitemap(
                 response.getOutputStream(),
-                ImmutableList.of("/experiments", "/plant/experiments"),
-                solr.getBioentityIdentifiersForSpecies(speciesFactory.create(species).getReferenceName()));
+                sitemapDao.getBioentityIdentifiersInPublicExperiments(species),
+                allEntries);
     }
-
 }
