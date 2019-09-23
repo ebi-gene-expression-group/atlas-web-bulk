49242141785bc8712d9b82646bba6dd1a9e423f5
amunozpomer <amunoz@ebi.ac.uk>
2019-09-23 16:15:21 +0100

    Set cache size on app init as number of experiment directories * 1.25

--- src/main/java/uk/ac/ebi/atlas/configuration/CacheConfig.java
+++ src/main/java/uk/ac/ebi/atlas/configuration/CacheConfig.java
@@ -1,32 +1,62 @@
 package uk.ac.ebi.atlas.configuration;
 
 import org.cache2k.extra.spring.SpringCache2kCacheManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.util.Arrays;
+
 @EnableCaching
 @Configuration
 public class CacheConfig {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CacheConfig.class);
+    private static final int DEFAULT_EXPERIMENT_DIR_COUNT = 100;
+    private Path experimentsDirPath;
+
+    public CacheConfig(Path experimentsDirPath) {
+        this.experimentsDirPath = experimentsDirPath;
+    }
+
     @Bean
     public CacheManager cacheManager() {
-        SpringCache2kCacheManager cacheManager = new SpringCache2kCacheManager();
-
-        cacheManager.addCaches(
+        return new SpringCache2kCacheManager().addCaches(
                 builder -> builder.name("designElementsByGeneId"),
                 builder -> builder.name("arrayDesignByAccession"),
                 builder -> builder.name("bioentityProperties"),
 
-                builder -> builder.name("experiment").eternal(true).entryCapacity(4000),
-                builder -> builder.name("experimentAttributes"),
-                builder -> builder.name("speciesSummary"),
+                builder ->
+                        builder.name("experiment")
+                                .eternal(true)
+                                .entryCapacity(
+                                        Double.valueOf(Math.ceil(1.25 * countExperimentDirectories())).intValue()),
+                builder -> builder.name("experimentAttributes").eternal(true),
+                builder -> builder.name("speciesSummary").eternal(true),
 
-                builder -> builder.name("experimentContent"),
+                builder -> builder.name("experimentContent").eternal(true),
 
                 // Used for sitemap.xml files
-                builder -> builder.name("publicBioentityIdentifiers"),
-                builder -> builder.name("publicSpecies"));
-        return cacheManager;
+                builder -> builder.name("publicBioentityIdentifiers").eternal(true),
+                builder -> builder.name("publicSpecies").eternal(true));
+    }
+
+    private long countExperimentDirectories() {
+        try {
+            long experimentDirCount = Arrays.stream(experimentsDirPath.resolve("magetab").toFile().listFiles())
+                    .filter(File::isDirectory)
+                    .map(File::getName)
+                    .filter(filename -> filename.startsWith("E-"))
+                    .count();
+            LOGGER.info("Found {} experiment directories", experimentDirCount);
+            return experimentDirCount;
+        } catch (Exception e) {
+            LOGGER.error("There was an error reading {}", experimentsDirPath.resolve("magetab").toString());
+            return DEFAULT_EXPERIMENT_DIR_COUNT;
+        }
     }
 }
