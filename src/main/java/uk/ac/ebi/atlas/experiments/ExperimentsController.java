4790e30bbac4dd6d0b06fb6a3e5c77fba388b8ed
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2020-03-06 13:06:57 +0000

    Bugfix #170309202 – Baseline differential links to experiment table don’t work (#58)
    * Update bundle to pre-release version of experiment table
    
    * Dev bundles
    
    * Integrate experiment table with routing

--- src/main/java/uk/ac/ebi/atlas/experiments/ExperimentsController.java
+++ src/main/java/uk/ac/ebi/atlas/experiments/ExperimentsController.java
@@ -3,28 +3,13 @@ package uk.ac.ebi.atlas.experiments;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import uk.ac.ebi.atlas.controllers.HtmlExceptionHandlingController;
 
 @Controller
 public class ExperimentsController extends HtmlExceptionHandlingController {
-
     @RequestMapping(value = "/experiments", produces = "text/html;charset=UTF-8")
-    public String getExperimentsListParameters(
-            @RequestParam(value = "experimentType", required = false) String experimentType,
-            @RequestParam(value = "kingdom", required = false) String kingdom,
-            @RequestParam(value = "species", required = false) String species,
-            @RequestParam(value = "experimentSet", required = false) String experimentSet,
-            Model model) {
-
-        model.addAttribute("experimentType", experimentType);
-        model.addAttribute("kingdom", kingdom);
-        model.addAttribute("species", species);
-        model.addAttribute("experimentSet", experimentSet);
-
+    public String getExperimentsListParameters(Model model) {
         model.addAttribute("mainTitle", "Experiments ");
-
         return "experiments";
     }
-
 }
