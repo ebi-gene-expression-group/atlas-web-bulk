cd9ff0b01cc3725de94be7c7ce8181883c8eb183
lingyun1010 <zhaolingyun1010@gmail.com>
2020-05-18 05:58:30 +0100

    Update the parse types for secondary accession

--- src/test/java/uk/ac/ebi/atlas/trader/GxaExperimentRepositoryTest.java
+++ src/test/java/uk/ac/ebi/atlas/trader/GxaExperimentRepositoryTest.java
@@ -16,7 +16,6 @@ import uk.ac.ebi.atlas.trader.factory.MicroarrayExperimentFactory;
 import uk.ac.ebi.atlas.trader.factory.RnaSeqDifferentialExperimentFactory;
 
 import java.sql.Timestamp;
-import java.util.Collections;
 import java.util.Date;
 import java.util.Random;
 import java.util.UUID;
@@ -88,7 +87,7 @@ class GxaExperimentRepositoryTest {
         when(idfParserMock.parse(experimentAccession))
                 .thenReturn(new IdfParserOutput(
                         randomAlphanumeric(500),
-                        Collections.emptyList(),
+                        ImmutableList.of(randomAlphanumeric(10)),
                         randomAlphanumeric(100),
                         ImmutableList.of(),
                         RNG.nextInt(50),
