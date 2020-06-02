90a0f587bacb14754eae4d2d15fa4f15050fd7fc
Haider Iqbal <haideri@ebi.ac.uk>
2020-06-02 16:47:52 +0100

    - Add proper annotation to ExperimentCollection class

--- src/main/java/uk/ac/ebi/atlas/experiments/ExperimentCollection.java
+++ src/main/java/uk/ac/ebi/atlas/experiments/ExperimentCollection.java
@@ -1,7 +1,10 @@
 package uk.ac.ebi.atlas.experiments;
 
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
+@Repository
 public class ExperimentCollection implements ExperimentCollectionsRepository {
 
     @Override
