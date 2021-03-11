d520aa2465fe8c186b0141088efe2bf89ade92e6
amunozpomer <amunoz@ebi.ac.uk>
2021-03-11 13:42:23 +0000

    Convert old for loops to stream operations

--- src/main/java/uk/ac/ebi/atlas/solr/analytics/AnalyticsSearchService.java
+++ src/main/java/uk/ac/ebi/atlas/solr/analytics/AnalyticsSearchService.java
@@ -4,14 +4,17 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.jayway.jsonpath.JsonPath;
 import net.minidev.json.JSONArray;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import uk.ac.ebi.atlas.search.SemanticQuery;
 import uk.ac.ebi.atlas.species.SpeciesFinder;
 
 import java.util.List;
 import java.util.Map;
 
-@Component
+import static com.google.common.collect.ImmutableList.toImmutableList;
+import static com.google.common.collect.ImmutableSet.toImmutableSet;
+
+@Service
 public class AnalyticsSearchService implements SpeciesFinder {
     private final MiscellaneousAnalyticsSearchDao miscellaneousAnalyticsSearchDao;
 
@@ -24,13 +27,10 @@ public class AnalyticsSearchService implements SpeciesFinder {
     }
 
     public static ImmutableSet<String> readBuckets(String response) {
-        List<Map<String, Object>> res = JsonPath.read(response, "$..buckets[*]");
-
-        ImmutableSet.Builder<String> b = ImmutableSet.builder();
-        for (Map<String, Object> m: res) {
-            b.add(m.get("val").toString());
-        }
-        return b.build();
+        return JsonPath.<List<Map<String, Object>>>read(response, "$..buckets[*]").stream()
+                .map(bucketMap -> bucketMap.get("val"))
+                .map(Object::toString)
+                .collect(toImmutableSet());
     }
 
     public ImmutableSet<String> fetchExperimentTypesInAnyField(SemanticQuery query) {
@@ -88,11 +88,12 @@ public class AnalyticsSearchService implements SpeciesFinder {
     }
 
     private static ImmutableList<String> readSpecies(String response) {
-        JSONArray res = JsonPath.read(response, "$..species[*]");
+        JSONArray speciesWithCounts = JsonPath.read(response, "$..species[*]");
 
+        // The JSON array is a list of species in even positions followed by a doc count in odd positions
         ImmutableList.Builder<String> b = ImmutableList.builder();
-        for (int i = 0; i < res.size(); i += 2) {
-            b.add(res.get(i).toString());
+        for (int i = 0; i < speciesWithCounts.size(); i += 2) {
+            b.add(speciesWithCounts.get(i).toString());
         }
         return b.build();
     }
