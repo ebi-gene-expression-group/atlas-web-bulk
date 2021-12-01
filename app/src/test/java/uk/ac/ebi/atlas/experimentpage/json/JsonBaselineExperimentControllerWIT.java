d8ec448da42a06eacf117868eca1e9c1d7051d2c
Upendra Kumbham <upendra.kumbham@gmail.com>
2021-12-01 16:42:19 +0000

    code format

---
+++ app/src/test/java/uk/ac/ebi/atlas/experimentpage/json/JsonBaselineExperimentControllerWIT.java
@@ -0,0 +1,22 @@
+package uk.ac.ebi.atlas.experimentpage.json;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JsonBaselineExperimentControllerWIT {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getsBaselineProteomicsDiaSwathExperimentData() {
+    }
+}
