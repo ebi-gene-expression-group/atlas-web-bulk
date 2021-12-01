c8815cf89eb516a5d2227c04ce20357887e41b9c
Pablo Moreno <pablo.a.moreno@gmail.com>
2021-12-01 09:11:09 +0000

    Avoid silent errors in coexpression profile loader.

--- cli/src/main/java/uk/ac/ebi/atlas/cli/coexpression/CoexpressionUpdateCommand.java
+++ cli/src/main/java/uk/ac/ebi/atlas/cli/coexpression/CoexpressionUpdateCommand.java
@@ -6,6 +6,8 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import uk.ac.ebi.atlas.experimentimport.coexpression.BaselineCoexpressionProfileLoader;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.logging.Logger;
@@ -33,14 +35,29 @@ public class CoexpressionUpdateCommand implements Callable<Integer> {
         int deleteCount;
         int loadCount;
 
+        int done=0;
+        List<String> failed_accessions = new ArrayList<>();
         for(String accession : experimentAccessions) {
             deleteCount = baselineCoexpressionProfileLoader.deleteCoexpressionsProfile(accession);
-            loadCount = baselineCoexpressionProfileLoader.loadBaselineCoexpressionsProfile(accession);
-            LOGGER.info(String.format(
-                    " deleted %, d and loaded %, d coexpression profiles for accession %s", deleteCount, loadCount, accession));
+            try {
+                loadCount = baselineCoexpressionProfileLoader.loadBaselineCoexpressionsProfile(accession, true);
+                done++;
+                LOGGER.info(String.format(
+                        " deleted %, d and loaded %, d coexpression profiles for accession %s", deleteCount, loadCount, accession));
+            } catch (IOException e) {
+                failed_accessions.add(accession);
+                LOGGER.severe(String.format("%s FAILED", accession));
+                LOGGER.severe(e.getMessage());
+            }
 
         }
-        return 0;
+        int failed = failed_accessions.size();
+        LOGGER.warning(String.format("% experiments failed", failed));
+        if (failed > 0) {
+            LOGGER.info(String.format("Re-run with the following arguments to re-try failed accessions: %s", String.join(",", failed_accessions)));
+        }
+        LOGGER.info(String.format("% experiments done", done));
+        return failed_accessions.size();
     }
 
 }
