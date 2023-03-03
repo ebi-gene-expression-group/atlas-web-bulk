b3abda1de4748dbbfeac67720148f9023fff63c9
Karoly Erdos <karoly@ebi.ac.uk>
2023-03-03 15:44:27 +0000

    Improve supporting script for unit test execution/debugging

--- execute-all-tests.sh
+++ execute-all-tests.sh
@@ -6,7 +6,6 @@ source ${SCRIPT_DIR}/docker/dev.env
 
 echo "Execute all tests"
 
-SCHEMA_VERSION=${1:-latest} \
 docker-compose \
 --env-file ${SCRIPT_DIR}/docker/dev.env \
 -f docker/docker-compose-postgres-test.yml \
@@ -16,9 +15,9 @@ run --rm --service-ports \
 gxa-gradle bash -c "
 set -e
 
-./gradlew clean
+gradle clean
 
-./gradlew \
+gradle \
 -PdataFilesLocation=/atlas-data \
 -PexperimentFilesLocation=/atlas-data/gxa \
 -PexperimentDesignLocation=/atlas-data/expdesign \
@@ -29,11 +28,11 @@ set -e
 -PsolrHosts=http://${SOLR_CLOUD_CONTAINER_1_NAME}:8983/solr,http://${SOLR_CLOUD_CONTAINER_2_NAME}:8983/solr \
 atlas-web-core:testClasses
 
-./gradlew -PtestResultsPath=ut :atlas-web-core:test --tests *Test
-#sh./gradlew -PtestResultsPath=it :atlas-web-core:test --tests *IT
-./gradlew :atlas-web-core:jacocoTestReport
+gradle -PtestResultsPath=ut :atlas-web-core:test --tests *Test
+#gradle -PtestResultsPath=it :atlas-web-core:test --tests *IT
+gradle :atlas-web-core:jacocoTestReport
 
-./gradlew \
+gradle \
 -PdataFilesLocation=/atlas-data \
 -PexperimentFilesLocation=/atlas-data/gxa \
 -PexperimentDesignLocation=/atlas-data/expdesign \
@@ -44,8 +43,8 @@ atlas-web-core:testClasses
 -PsolrHosts=http://${SOLR_CLOUD_CONTAINER_1_NAME}:8983/solr,http://${SOLR_CLOUD_CONTAINER_2_NAME}:8983/solr \
 app:testClasses
 
-./gradlew -PtestResultsPath=ut :app:test --tests *Test
-./gradlew -PtestResultsPath=it -PexcludeTests=**/*WIT.class :app:test --tests *IT
-./gradlew -PtestResultsPath=e2e :app:test --tests *WIT
-./gradlew :app:jacocoTestReport
+gradle -PtestResultsPath=ut :app:test --tests *Test
+gradle -PtestResultsPath=it -PexcludeTests=**/*WIT.class :app:test --tests *IT
+gradle -PtestResultsPath=e2e :app:test --tests *WIT
+gradle :app:jacocoTestReport
 "
--- execute-single-test.sh
+++ execute-single-test.sh
@@ -4,7 +4,49 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 source ${SCRIPT_DIR}/docker/dev.env
 
-TEST_CASE_NAME=$1
+function print_usage() {
+  printf '\n%b\n\n' "Usage: ${0} [ -a APPLICATION_NAME ] [ -s SCHEMA_VERSION ]"
+  printf '%b\n' "Executing a given unit/integration test from the given application using the given schema version"
+  printf '\n%b\n' "-p\tName of the project the test can be found\tfor example: app or atlas-web-core"
+  printf '%b\n\n' "-h\tShow usage instructions"
+}
+
+PROJECT_NAME=app
+SCHEMA_VERSION=latest
+mandatory_name=false
+
+while getopts "n:p:h" opt
+do
+  case ${opt} in
+    n )
+      mandatory_name=true; TEST_CASE_NAME=${OPTARG}
+      ;;
+    p )
+      PROJECT_NAME=${OPTARG}
+      if ! [[ "$PROJECT_NAME" =~ ^(app|atlas-web-core)$ ]]; then
+        echo "Project name is not valid -$OPTARG" >&2
+        exit 1
+      fi
+      ;;
+    h )
+      print_usage
+      exit 0
+      ;;
+    \?)
+      printf '%b\n' "Invalid option: -${OPTARG}" >&2
+      print_usage
+      exit 2
+      ;;
+    : ) echo "Missing option argument for: $OPTARG" >&2; exit 1;;
+  esac
+done
+
+if ! $mandatory_name
+then
+    echo "-n must be provided with the name of the test to execute" >&2
+    exit 1
+fi
+
 echo "Testing ${TEST_CASE_NAME}"
 
 docker-compose \
@@ -26,7 +68,7 @@ gradle \
 -PjdbcPassword=${POSTGRES_PASSWORD} \
 -PzkHosts=${SOLR_CLOUD_ZK_CONTAINER_1_NAME}:2181,${SOLR_CLOUD_ZK_CONTAINER_2_NAME}:2181,${SOLR_CLOUD_ZK_CONTAINER_3_NAME}:2181 \
 -PsolrHosts=http://${SOLR_CLOUD_CONTAINER_1_NAME}:8983/solr,http://${SOLR_CLOUD_CONTAINER_2_NAME}:8983/solr \
-app:testClasses
+${PROJECT_NAME}:testClasses
 
-gradle --continuous :app:test --tests $TEST_CASE_NAME
+gradle --continuous :${PROJECT_NAME}:test --tests $TEST_CASE_NAME
 "
