48b56bbecfa15ac6b72057347ae7f23f06cb890e
Haider Iqbal <haideri@ebi.ac.uk>
2023-03-28 14:41:47 +0100

    - Add ExperimentDesignController and test

---
+++ app/src/main/java/uk/ac/ebi/atlas/experiments/ExperimentDesignController.java
@@ -0,0 +1,33 @@
+package uk.ac.ebi.atlas.experiments;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import uk.ac.ebi.atlas.controllers.JsonExceptionHandlingController;
+import uk.ac.ebi.atlas.model.experiment.ExperimentDesignTableService;
+
+import static uk.ac.ebi.atlas.utils.GsonProvider.GSON;
+
+@RestController
+public class ExperimentDesignController extends JsonExceptionHandlingController {
+    private final ExperimentDesignTableService experimentDesignService;
+
+    public ExperimentDesignController (ExperimentDesignTableService experimentDesignService) {
+        this.experimentDesignService = experimentDesignService;
+    }
+
+    @GetMapping(value = "/json/experiment-design/{experiment_accession}",
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public String getExperimentDesign(@PathVariable String experiment_accession,
+                                      @RequestParam(value = "pageNo", defaultValue = "1") int pageNo,
+                                      @RequestParam(value = "pageSize", defaultValue = "20") int pageSize) {
+        if(pageNo < 1)
+            throw new IllegalArgumentException("Page number must be greater than 0");
+        if(pageSize < 1)
+            throw new IllegalArgumentException("Page size must be greater than 0");
+
+        return  GSON.toJson(experimentDesignService.getExperimentDesignData(experiment_accession, pageNo, pageSize));
+    }
+}
