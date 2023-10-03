17a8b4be85a4e9385214d6fd7107a9b2ee0af33d
Karoly Erdos <karoly@ebi.ac.uk>
2023-10-03 16:38:04 +0100

    Remove unneeded comments

--- app/src/test/java/uk/ac/ebi/atlas/configuration/TestConfig.java
+++ app/src/test/java/uk/ac/ebi/atlas/configuration/TestConfig.java
@@ -2,16 +2,12 @@ package uk.ac.ebi.atlas.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 // Enabling component scanning will also load BasePathsConfig, JdbcConfig and SolrConfig, so just using this class as
-// application context is enough in integration tests. It’s important to exclude CacheConfig, otherwise Spring will
-// complain if you want to inject classes such as GxaExperimentTrader, since a proxy will be injected instead! As an
-// exercise, remove CacheConfig.class and run tests in GxaExperimentTraderIT.
+// application context is enough in integration tests.
 @ComponentScan(basePackages = "uk.ac.ebi.atlas")
 public class TestConfig {
     @Bean
