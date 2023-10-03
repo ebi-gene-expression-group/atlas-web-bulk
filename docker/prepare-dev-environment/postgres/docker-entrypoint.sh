8866ef746d960030e3ff6209c4b2cdd6881caf2b
Karoly Erdos <karoly@ebi.ac.uk>
2023-10-03 16:18:49 +0100

    Revert both docker changes as already applied these in an already merged branch

--- docker/prepare-dev-environment/postgres/docker-entrypoint.sh
+++ docker/prepare-dev-environment/postgres/docker-entrypoint.sh
@@ -17,15 +17,3 @@ cd /root/atlas-web-bulk
 -PjdbcUsername=${POSTGRES_USER} \
 -PjdbcPassword=${POSTGRES_PASSWORD} \
 :cli:bootRun --args="update-baseline-coexpression -e $(echo ${EXP_IDS} | sed -e "s/ /,/g")"
-
-cd /root/db-scxa/bin
-export PATH=.:${PATH}
-export dbConnection=postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:5432/${POSTGRES_DB}
-
-for EXP_ID in ${EXP_IDS}
-do
-  EXP_ID=${EXP_ID} \
-  CONDENSED_SDRF_FILE=/atlas-data/gxa/magetab/${EXP_ID}/${EXP_ID}.condensed-sdrf.tsv \
-  SDRF_FILE=/atlas-data/gxa/magetab/${EXP_ID}/${EXP_ID}.sdrf.txt \
-  ./load_exp_design.sh
-done
