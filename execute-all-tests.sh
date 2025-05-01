a6cdf1cd22436cc27b7701ad0e7e5f65a49e0ccf
upendrakumbham <upendra.kumbham@gmail.com>
2025-05-01 15:08:06 +0100

    Uncomment atlas-web-core IT tests and fix related code to run tests.Use latest docker.
    Signed-off-by: upendrakumbham <upendra.kumbham@gmail.com>

--- execute-all-tests.sh
+++ execute-all-tests.sh
@@ -6,7 +6,7 @@ source ${SCRIPT_DIR}/docker/dev.env
 
 echo "Execute all tests"
 
-docker-compose \
+docker compose \
 --env-file ${SCRIPT_DIR}/docker/dev.env \
 -f docker/docker-compose-postgres-test.yml \
 -f docker/docker-compose-solrcloud.yml \
@@ -24,12 +24,12 @@ gradle \
 -PjdbcUrl=jdbc:postgresql://${POSTGRES_HOST}:5432/${POSTGRES_DB} \
 -PjdbcUsername=${POSTGRES_USER} \
 -PjdbcPassword=${POSTGRES_PASSWORD} \
--PzkHosts=${SOLR_CLOUD_ZK_CONTAINER_1_NAME}:2181,${SOLR_CLOUD_ZK_CONTAINER_2_NAME}:2181,${SOLR_CLOUD_ZK_CONTAINER_3_NAME}:2181 \
--PsolrHosts=http://${SOLR_CLOUD_CONTAINER_1_NAME}:8983/solr,http://${SOLR_CLOUD_CONTAINER_2_NAME}:8983/solr \
+-PzkHosts=${PROJECT_NAME}-${SOLR_CLOUD_ZK_CONTAINER_1_NAME}:2181,${PROJECT_NAME}-${SOLR_CLOUD_ZK_CONTAINER_2_NAME}:2181,${PROJECT_NAME}-${SOLR_CLOUD_ZK_CONTAINER_3_NAME}:2181 \
+-PsolrHosts=http://${PROJECT_NAME}-${SOLR_CLOUD_CONTAINER_1_NAME}:8983/solr,http://${PROJECT_NAME}-${SOLR_CLOUD_CONTAINER_2_NAME}:8983/solr \
 atlas-web-core:testClasses
 
 gradle -PtestResultsPath=ut :atlas-web-core:test --tests *Test
-#gradle -PtestResultsPath=it :atlas-web-core:test --tests *IT
+gradle -PtestResultsPath=it :atlas-web-core:test --tests *IT
 gradle :atlas-web-core:jacocoTestReport
 
 gradle \
@@ -49,7 +49,7 @@ gradle -PtestResultsPath=e2e :app:test --tests *WIT
 gradle :app:jacocoTestReport
 "
 
-docker-compose \
+docker compose \
 --env-file ${SCRIPT_DIR}/docker/dev.env \
 -f docker/docker-compose-postgres-test.yml \
 -f docker/docker-compose-solrcloud.yml \
